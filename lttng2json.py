8dfbceec4170773fba9817115be3b6452d70a7b6
Engel A. Sanchez <engel@basho.com>
2013-09-03 14:07:42 -0400

    WIP on adding more visual events
    Had to check this in to switch to reviewing Core/KV stuff

--- lttng2json.py
+++ lttng2json.py
@@ -5,11 +5,11 @@ import os, re, json, sys
 from collections import namedtuple
 
 TraceEvent = namedtuple('TraceEvent',
-        ['secs', 'nsecs', 'name', 'cpu', 'pid', 'tid', 'procname', 'args'])
+        ['timestamp', 'secs', 'nsecs', 'name', 'cpu', 'pid', 'tid', 'procname', 'args'])
 
 rx = re.compile(r"""^
     # 24hr based timestamp
-    \[(?P<hours>\d\d):(?P<mins>\d\d):(?P<secs>\d\d)\.(?P<nsecs>\d{9})\]
+    \[(?P<timestamp>(?P<hours>\d\d):(?P<mins>\d\d):(?P<secs>\d\d)\.(?P<nsecs>\d{9}))\]
     \s+
     \([^)]+\)   # Time relative to last event
     \s+
@@ -25,8 +25,6 @@ rx = re.compile(r"""^
     \s*$
     """, re.X)
 
-start_secs = None 
-start_nsecs = None
 secs_in_day = 24 * 60 * 60
 
 def time_diff(secs1, nsecs1, secs2, nsecs2):
@@ -57,8 +55,43 @@ def to_dict(str):
             result[name] = m.group(5)
     return result
 
-def event_iter():
-    global start_secs, start_nsecs
+class ErlangThread:
+    def __init__(self, num):
+        self.running = False
+        self.secs = None
+        self.nsecs = None
+        self.last_time = None
+        self.last_event = None
+        self.in_syscall = None
+
+class Scheduler(ErlangThread):
+    def __init__(self, num):
+        ErlangThread.__init__(self, num)
+        self.number = num
+        self.process = None
+
+class CPU:
+    def __init__(self, num):
+        self.thread = None
+        self.number = num
+
+class TraceState:
+    """
+    State of the world as trace events are parsed, including
+    what schedulers are running and on what CPUs, etc
+    """
+    def __init__(self, num_cpus, tid2sched):
+        self.start_secs = None
+        self.start_nsecs = None
+        self.scheds = {tid: Scheduler(snum)\
+                for tid, snum in tid2sched.iteritems() }
+        self.cpus = [ CPU(n) for n in range(0, num_cpus) ]
+
+def event_iter(state):
+    """
+    Converts output from Babeltrace in stdin into
+    a stream of TraceEvent objects
+    """
     for line in sys.stdin:
         #print("Original line is ", line)
         m = rx.match(line)
@@ -69,8 +102,10 @@ def event_iter():
             mins = int(m.group("mins"))
             secs = int(m.group("secs"))
             nsecs = int(m.group("nsecs"))
+            timestamp = m.group("timestamp")
             day_secs = secs + (mins + hours * 60) * 60
             event = TraceEvent(
+                    timestamp = timestamp,
                     secs = day_secs,
                     nsecs = nsecs,
                     name = m.group("name"),
@@ -80,9 +115,9 @@ def event_iter():
                     procname = ctx["procname"],
                     args = args
                     )
-            if start_secs is None:
-                start_secs = day_secs
-                start_nsecs = nsecs
+            if state.start_secs is None:
+                state.start_secs = day_secs
+                state.start_nsecs = nsecs
             yield event
         else:
             print("Could not parse line : ", line, file=sys.stderr)
@@ -102,46 +137,92 @@ def load_sched_tids(fname):
     #print("Tids results", result)
     return result
 
-def do_sched_switch(ev, scheds):
+def exit_sched_event(sched, ev):
+    p = sched.process
+    dt = time_diff(sched.secs, sched.nsecs, ev.secs, ev.nsecs) 
+    t = sched.last_time
+    snum = sched.number
+    if p:
+        return (snum,
+                {'cl':'p', 't':t, 'dt':dt, 'x':{'pid':p['pid']}})
+    else:
+        dbg = "%s -> %s" % (sched.last_event, ev)
+        o = {'cl':'s', 't':t, 'dt':dt, 'dbg':dbg}
+        print("Event %s from %s" % (o, dbg))
+        return (snum, o)
+
+def do_sched_switch(ev, state):
     prev_tid = ev.args['prev_tid']
     next_tid = ev.args['next_tid']
     out = []
 
-    if prev_tid in scheds:
-        #print("Matched prev_tid on ", ev)
-        s = scheds[prev_tid]
-        if s['running']:
-            p = s.get('process')
-            dt = time_diff(s['secs'], s['nsecs'], ev.secs, ev.nsecs) 
-            t = s['last_time']
-            snum = s['number']
-            if p:
-                out.append((snum, {'cl':'p', 't':t, 'dt':dt, 'x':{'pid':p['pid']} }))
-            else:
-                out.append((snum, {'cl':'s', 't':t, 'dt':dt}))
-            s['running'] = False
+    if prev_tid in state.scheds:
+        print("Matched prev_tid on ", ev)
+        sched = state.scheds[prev_tid]
+        if sched.running:
+            block = exit_sched_event(sched, ev)
+            print("======= exiting thread:", block)
+            sched.running = False
+            state.cpus[ev.cpu].thread = None
+            out.append(block)
+        else:
+            print("thread not running")
 
-    if next_tid in scheds:
-        #print("Matched next_tid on ", ev)
-        s = scheds[next_tid]
-        print("Setting time ", start_secs, start_nsecs, ev.secs, ev.nsecs)
-        s['last_time'] = time_diff(start_secs, start_nsecs, ev.secs, ev.nsecs)
-        s['secs'] = ev.secs
-        s['nsecs'] = ev.nsecs
-        s['running'] = True  # But maybe not in userspace yet until syscall exit
+    if next_tid in state.scheds:
+        print("Matched next_tid on ", ev)
+        sched = state.scheds[next_tid]
+        sched.last_time = time_diff(state.start_secs,
+                state.start_nsecs, ev.secs, ev.nsecs)
+        sched.last_event = ev
+        sched.secs = ev.secs
+        sched.nsecs = ev.nsecs
+        sched.running = True 
+
+        state.cpus[ev.cpu].thread = sched
 
     return out
 
-def do_process_scheduled(event, scheds):
+def do_process_scheduled(ev, state):
     return []
 
-def do_process_unscheduled(event, scheds):
+def do_process_unscheduled(ev, state):
     return []
 
-def do_irq_entry(event, scheds):
+def do_syscall_entry(ev, state):
+    thread = state.cpus[ev.cpu].thread
+    if thread:
+        block = exit_sched_event(thread, ev)
+        print("======= Erlang thread block:", block)
+        thread.in_syscall = ev
+        return [block]
+    else:
+        print("no erlang thread for syscall")
     return []
 
-def do_irq_exit(event, scheds):
+def do_syscall_exit(ev, state):
+    thread = state.cpus[ev.cpu].thread
+    if thread:
+        start_ev = thread.in_syscall
+        if start_ev:
+            thread.in_syscall = None
+            t = time_diff(state.start_secs, state.start_nsecs,
+                ev.secs, ev.nsecs)
+            thread.last_time = t
+            dt = time_diff(start_ev.secs, start_ev.nsecs,
+                ev.secs, ev.nsecs)
+            dbg = "%s -> %s" % (start_ev, ev)
+            block = {'cl':'sc','t':t,'dt':dt,'n':start_ev.name, 'dbg':dbg}
+            print('===== syscall block:', block)
+            return [(thread.number, block)]
+
+    else:
+        print('no erlang thread for syscall')
+    return []
+
+def do_irq_entry(ev, state):
+    return []
+
+def do_irq_exit(ev, state):
     return []
 
 def visual_blocks_iter(sched_tids):
@@ -154,8 +235,7 @@ def visual_blocks_iter(sched_tids):
         - Enter with erlang:process_scheduled
         - Exit with beam scheduler exit and erlang:process_unscheduled
     """
-    scheds = {tid:{'number':snum, 'running':False} for tid, snum in sched_tids.iteritems() }
-    #print("Scheds ", scheds)
+    state = TraceState(16, sched_tids)
     handlers = {
             'sched_switch': do_sched_switch,
             'erlang:process_scheduled': do_process_scheduled,
@@ -163,39 +243,50 @@ def visual_blocks_iter(sched_tids):
             'irq_handler_entry':do_irq_entry,
             'irq_handler_exit':do_irq_exit,
             'softirq_entry':do_irq_entry,
-            'softirq_exit':do_irq_exit
+            'softirq_exit':do_irq_exit,
+            'exit_syscall':do_syscall_exit
             }
 
-    for e in event_iter():
+    for e in event_iter(state):
         handler = handlers.get(e.name)
+        if not handler and e.name.startswith('sys_'):
+            handler = do_syscall_entry
         if handler:
-            for el in handler(e, scheds):
+            print(handler.func_name, ":", e)
+            for el in handler(e, state):
                 yield el
+        else:
+            print('no handler: ', e)
 
 
 tids_file = sys.argv[1]
 sname = sys.argv[2] if len(sys.argv) > 2 else "default"
-os.mkdir(sname)
+if not os.path.isdir(sname):
+    os.mkdir(sname)
 tids = load_sched_tids(tids_file)
 s_files = ['dummy0']
-s_file_started = {}
+started_files = set() 
 
 try:
     for sn in range(1,len(tids)+1):
         s_files.append(open("%s/sched%d.json" % (sname, sn), 'w'))
     for s, block in visual_blocks_iter(tids):
         f = s_files[s]
-        if s_file_started.get(s, False):
+        if f in started_files:
             print(',', file=f)
         else:
             print('{"data":[', file=f)
-            s_file_started[s] = True
+            started_files.add(f)
         json.dump(block, f)
 finally:
     for f in s_files:
         try:
-            print('\n]}', file=f)
-            f.close()
+            if f in started_files:
+                print('\n]}', file=f)
+                f.close()
+            else:
+                f.close()
+                os.remove(f.name)
         except:
             pass
 
--- tpl/main.js
+++ tpl/main.js
@@ -1,32 +1,70 @@
-
+(function() {
 
 var w = 1000,
     h = 600,
-    sh = 40,
-    timeWindow = 1000,
-    tScale = d3.scale.linear().range([0, w]).domain([0, timeWindow]);
+    strip_h = 40,
+    margin = 15,
+    totW = w + 2 * margin,
+    lblDx = 100,
+    axisHeight = 40;
+
+var tScale = d3.scale.linear()
+    .range([0, w])
+    .domain([0, trace_data.duration]);
 
 var vis = d3.select("#body").append("div")
-.attr("class", "chart")
-.style("width", (w+200) + "px")
-.style("height", h + "px")
-.append("svg:svg")
-.attr("width", w+200)
-.attr("height", h);
+    .attr("class", "chart")
+    .style("width", (totW) + "px")
+    .style("height", h + "px")
+    .append("svg:svg")
+    .attr("width", totW)
+    .attr("height", h)
+    .append("svg:g")
+    .attr("transform", "translate("+margin+",0)");
 
-var tAxis = d3.svg.axis().scale(tScale).orient("top").ticks(10).tickSize(20);
+var zoom = d3.behavior.zoom()
+    .scaleExtent([1,10])
+    .on("zoom", draw);
 
-function load_data (root) {
-    var sg = vis.append("svg:g").attr("class", "sched-strip")
-        .attr("transform", "translate(100, 40)");
+zoom.x(tScale);
 
-    var g = sg.selectAll("g").data(root)
+var tAxis = d3.svg.axis().scale(tScale).orient("top")
+    .ticks(10).tickFormat(fmtTime(0)).tickSize(20);
+
+var mg = vis.append("g")
+    .attr("class", "g-main")
+    .attr("transform", "translate(0,"+axisHeight+")")
+    .call(zoom)
+    ;
+
+var ag = vis.append("g")
+    .attr("class", "g-axis")
+    .attr("transform", "translate(0,"+axisHeight+")")
+    .call(tAxis)
+    ;
+
+function draw(){
+    tAxis(ag);
+}
+
+draw();
+
+function load_strip (strip, strip_i) {
+    var sg = mg.append("svg:g")
+        .attr("class", "g-strip")
+        .attr("transform", "translate(0, "+(strip_h*strip_i)+")");
+
+    var g = sg.selectAll("g").data(strip.data)
         .enter().append("svg:g")
-        .attr("transform", function(d) { return "translate("+tScale(d.t)+",0)"});
+        .attr("class", function(d){return d.cl;})
+        .attr("transform", function(d) {
+            var x=tScale(d.t), y = strip_h * strip_i; 
+            return "translate("+x+","+y+")"
+        });
 
     g.append("svg:rect")
         .attr("width", function(d){return tScale(d.dt)})
-        .attr("height", sh)
+        .attr("height", strip_h)
         .attr("class", "event"); 
 
     g.append("svg:text")
@@ -35,16 +73,36 @@ function load_data (root) {
         .attr("class", "event-name")
         .text(function(d) { return d.name; });
 
-    vis.append("g").attr("class", "axis").attr("transform", "translate(100,40)").call(tAxis);
+    var fmt = fmtTime(1);
+    g.append("title").text(function(d){
+        return "t="+d.t+", dt="+fmt(d.dt)+", dbg="+d.dbg;});
+
     function transform(d) {
-        return "translate("+tScale(d.dt)/2+"," + sh / 2 + ")";
+        return "translate("+tScale(d.dt)/2+"," + strip_h / 2 + ")";
     }
 };
 
-var data = [
-{"name": "proc1", "t":0, "dt":300},
-{"name": "proc2", "t": 300, "dt":400},
-    ]; 
+trace_data.threads.forEach(function(thread, idx){
+    d3.json(thread.file, function(strip){
+        console.log("Loaded json ", strip);
+        load_strip(strip, idx);
+    });
+});
 
-load_data(data)
+function fmtTime(p) {
+    return function(f) {
+    var af = Math.abs(f);
+    if ( af >= 1.0 ){
+        return f.toFixed(p)+"s";
+    } else if ( af >= 1e-3 ) {
+        return (f*1e3).toFixed(p)+"ms";
+    } else if ( af >= 1e-6 ) {
+        return (f*1e6).toFixed(p)+"us";
+    } else {
+        return (f*1e9).toFixed(0)+"ns";
+    }
+    };
+}
+
+})();
 
