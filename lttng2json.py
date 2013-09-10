c33b9af7a0f2282e78856df308026f922e84a171
Engel A. Sanchez <engel@basho.com>
2013-09-09 21:21:22 -0400

    Add processes and port tasks
    Also rearranged the generated SVG elements: the initial implementation
    split them up into non-overlapping tiny bits as an early optimization
    that lead to horrible and slow code, as usual.  Now blocks are output
    after the blocks that contain them. SVG layering works by drawing
    objects that come later draw on top, so this is what we want. This will
    allow for easy layering of the visual elements, so you can for example
    turn off all system calls or interrupts, etc.

--- lttng2json.py
+++ lttng2json.py
@@ -4,6 +4,7 @@ from __future__ import print_function
 import os, re, json, sys
 from collections import namedtuple
 
+
 TraceEvent = namedtuple('TraceEvent',
         ['timestamp', 'secs', 'nsecs', 'name', 'cpu', 'pid', 'tid', 'procname', 'args'])
 
@@ -55,6 +56,78 @@ def to_dict(str):
             result[name] = m.group(5)
     return result
 
+class Span:
+    """
+    A time span defined by an opening and a close event.
+    For example, entering and eventually exiting a system call.
+    If across a user process, the opening event is the process being
+    scheduled in, and the start_event is the original entry.
+    """
+    def __init__(self, open_event):
+        self.open_event = open_event
+        self.start_event = open_event
+        self.close_event = None
+        self.children = []
+
+    def close(self, close_event):
+        self.close_event = close_event
+
+    def output_iter(self, state):
+        dt = time_diff(self.open_event.secs, self.open_event.nsecs,
+                self.close_event.secs, self.close_event.nsecs)
+        t = time_diff(state.start_secs, state.start_nsecs,
+                self.open_event.secs, self.open_event.nsecs)
+        dbg = "%s -> %s" % (self.open_event, self.close_event)
+        o = {'t':t, 'dt':dt, 'dbg':dbg}
+        self.decorate(o)
+        yield o
+        for child in self.children:
+            for p in child.output_iter(state):
+                yield p
+
+class SchedSpan(Span):
+    def __init__(self, ev1):
+        Span.__init__(self, ev1)
+
+    def decorate(self, o):
+        o['cl'] = 's'
+
+class ProcessSpan(Span):
+    def __init__(self, ev1):
+        Span.__init__(self, ev1)
+
+    def decorate(self, o):
+        o['cl'] = 'p'
+        o['pid'] = self.start_event.args['proc']
+
+class PortSpan(Span):
+    def __init__(self, ev1):
+        Span.__init__(self, ev1)
+
+    def decorate(self, o):
+        o['cl'] = 't'
+
+class SyscallSpan(Span):
+    def __init__(self, ev1):
+        Span.__init__(self, ev1)
+
+    def decorate(self, o):
+        o['cl'] = 'c'
+
+class HIRQSpan(Span):
+    def __init__(self, ev1):
+        Span.__init__(self, ev1)
+
+    def decorate(self, o):
+        o['cl'] = 'h'
+
+class SIRQSpan(Span):
+    def __init__(self, ev1):
+        Span.__init__(self, ev1)
+
+    def decorate(self, o):
+        o['cl'] = 'f'
+
 class ErlangThread:
     def __init__(self, num):
         self.running = False
@@ -62,6 +135,22 @@ class ErlangThread:
         self.nsecs = None
         self.last_event = None
         self.in_syscall = None
+        self.stack = []
+        self.span = None
+
+    def sched_out(self, sched_out_event):
+        self.running = False
+        for span in self.stack:
+            span.close_event = sched_out_event
+
+    def sched_in(self, sched_in_event):
+        self.running = True
+
+    def get_events_iter(self, state):
+        return self.span.output_iter(state)
+
+    def current_span(self):
+        return self.stack[-1] if len(self.stack) else None
 
 class Scheduler(ErlangThread):
     def __init__(self, num, tid):
@@ -69,6 +158,11 @@ class Scheduler(ErlangThread):
         self.number = num
         self.process = None
         self.tid = tid
+
+    def sched_in(self, sched_in_event):
+        ErlangThread.sched_in(self, sched_in_event)
+        self.span = SchedSpan(sched_in_event)
+        self.stack.append(self.span)
 
 class CPU:
     def __init__(self, num):
@@ -80,12 +174,12 @@ class TraceState:
     State of the world as trace events are parsed, including
     what schedulers are running and on what CPUs, etc
     """
-    def __init__(self, num_cpus, tid2sched):
+    def __init__(self, tid2sched):
         self.start_secs = None
         self.start_nsecs = None
         self.scheds = {tid: Scheduler(snum, tid)\
                 for tid, snum in tid2sched.iteritems() }
-        self.cpus = [ CPU(n) for n in range(0, num_cpus) ]
+        self.cpus = [ CPU(n) for n in range(0, 128) ]
 
 def event_iter(state):
     """
@@ -93,7 +187,7 @@ def event_iter(state):
     a stream of TraceEvent objects
     """
     for line in sys.stdin:
-        #print("Original line is ", line)
+        print("Original line is ", line)
         m = rx.match(line)
         if m:
             ctx = to_dict(m.group("context"))
@@ -136,97 +230,112 @@ def load_sched_tids(fname):
                 result[int(m.group(2))] = int(m.group(1))
     return result
 
-def exit_sched_event(sched, ev, state):
-    p = sched.process
-    dt = time_diff(sched.last_event.secs, sched.last_event.nsecs,
-            ev.secs, ev.nsecs)
-    t = time_diff(state.start_secs, state.start_nsecs,
-            sched.last_event.secs, sched.last_event.nsecs)
-    snum = sched.number
-    if p:
-        return (snum,
-                {'cl':'p', 't':t, 'dt':dt, 'x':{'pid':p['pid']}})
-    else:
-        dbg = "%s -> %s" % (sched.last_event, ev)
-        o = {'cl':'s', 't':t, 'dt':dt, 'dbg':dbg}
-        return (snum, o)
-
 def do_sched_switch(ev, state):
     prev_tid = ev.args['prev_tid']
     next_tid = ev.args['next_tid']
     out = []
 
     if prev_tid in state.scheds:
-        print("Matched prev_tid on ", ev)
+        #print("Matched prev_tid on ", ev)
         sched = state.scheds[prev_tid]
         if sched.running:
-            syscall = sched.in_syscall
-            if syscall:
-                t = time_diff(state.start_secs, state.start_nsecs,
-                    syscall.secs, syscall.nsecs)
-                dt = time_diff(syscall.secs, syscall.nsecs,
-                    ev.secs, ev.nsecs)
-                dbg = "%s -> %s" % (syscall, ev)
-                block = {'cl':'sc','t':t,'dt':dt,'n':syscall.name, 'dbg':dbg}
-                print('===== syscall interrupted block:', ev, block)
-                out.append((sched.number, block))
-            else:
-                block = exit_sched_event(sched, ev, state)
-                print("======= exiting thread:", block)
-                state.cpus[ev.cpu].thread = None
-                out.append(block)
-            sched.running = False
+            state.cpus[ev.cpu].thread = None
+            sched.sched_out(ev)
+            for b in sched.get_events_iter(state):
+                out.append((sched.number, b))
         else:
-            print("thread not running")
+            print("scheduler", sched, "not running")
 
     if next_tid in state.scheds:
-        print("Matched next_tid on ", ev)
+        #print("Matched next_tid on ", ev)
         sched = state.scheds[next_tid]
-        sched.last_event = ev
-        sched.running = True
-
+        sched.sched_in(ev)
         state.cpus[ev.cpu].thread = sched
 
     return out
 
+def do_generic_open(ev, state, constructor):
+    thread = state.cpus[ev.cpu].thread
+    if thread and thread.running and ev.tid == thread.tid:
+        current = thread.current_span()
+        if current:
+            s = constructor(ev)
+            current.children.append(s)
+            thread.stack.append(s)
+
+def do_generic_close(ev, state, span_type):
+    thread = state.cpus[ev.cpu].thread
+    if thread and thread.running and ev.tid == thread.tid:
+        current = thread.current_span()
+        if isinstance(current, span_type):
+            current.close(ev)
+            thread.stack.pop()
+
 def do_process_scheduled(ev, state):
+    print("Process schedule", ev)
+    do_generic_open(ev, state, ProcessSpan)
     return []
 
 def do_process_unscheduled(ev, state):
+    print("Process unschedule", ev)
+    do_generic_close(ev, state, ProcessSpan)
+    return []
+
+def do_port_begin(ev, state):
+    print("Port start", ev)
+    do_generic_open(ev, state, PortSpan)
+    return []
+
+def do_port_end(ev, state):
+    print("Port end", ev)
+    do_generic_close(ev, state, PortSpan)
     return []
 
 def do_syscall_entry(ev, state):
-    thread = state.cpus[ev.cpu].thread
-    if thread and thread.running and ev.tid == thread.tid:
-        block = exit_sched_event(thread, ev, state)
-        print("======= Erlang thread block during syscall:", ev, block)
-        thread.in_syscall = ev
-        thread.last_event = ev
-        return [block]
+    do_generic_open(ev, state, SyscallSpan)
     return []
 
 def do_syscall_exit(ev, state):
-    thread = state.cpus[ev.cpu].thread
-    if thread and thread.running and thread.tid == ev.tid:
-        syscall = thread.in_syscall
-        if syscall:
-            last_ev = thread.last_event
-            thread.in_syscall = None
-            t = time_diff(state.start_secs, state.start_nsecs,
-                    last_ev.secs, last_ev.nsecs)
-            thread.last_event = ev
-            dt = time_diff(last_ev.secs, last_ev.nsecs, ev.secs, ev.nsecs)
-            dbg = "%s -> %s" % (last_ev, ev)
-            block = {'cl':'sc','t':t,'dt':dt,'n':syscall.name, 'dbg':dbg}
-            print('===== syscall block:', ev, block)
-            return [(thread.number, block)]
+    do_generic_close(ev, state, SyscallSpan)
     return []
 
-def do_irq_entry(ev, state):
+def do_hirq_entry(ev, state):
+    do_generic_open(ev, state, HIRQSpan)
     return []
 
-def do_irq_exit(ev, state):
+def do_hirq_exit(ev, state):
+    do_generic_close(ev, state, HIRQSpan)
     return []
+
+def do_sirq_entry(ev, state):
+    do_generic_open(ev, state, SIRQSpan)
+    return []
+
+def do_sirq_exit(ev, state):
+    do_generic_close(ev, state, SIRQSpan)
+    return []
+
+class Handlers:
+    handlers = {
+            'sched_switch': do_sched_switch,
+            'erlang:process_scheduled': do_process_scheduled,
+            'erlang:process_unscheduled': do_process_unscheduled,
+            'erlang:begin_port_tasks': do_port_begin,
+            'erlang:end_port_tasks': do_port_end,
+            'irq_handler_entry':do_hirq_entry,
+            'irq_handler_exit':do_hirq_exit,
+            'softirq_entry':do_sirq_entry,
+            'softirq_exit':do_sirq_exit,
+            'exit_syscall':do_syscall_exit
+            }
+
+    @classmethod
+    def get_handler(cls, event_name):
+        handler = Handlers.handlers.get(event_name)
+        if not handler and event_name.startswith('sys_'):
+            return do_syscall_entry
+        return handler
+
 
 def visual_blocks_iter(sched_tids):
     """
@@ -238,24 +347,12 @@ def visual_blocks_iter(sched_tids):
         - Enter with erlang:process_scheduled
         - Exit with beam scheduler exit and erlang:process_unscheduled
     """
-    state = TraceState(16, sched_tids)
-    handlers = {
-            'sched_switch': do_sched_switch,
-            'erlang:process_scheduled': do_process_scheduled,
-            'erlang:process_unscheduled': do_process_unscheduled,
-            'irq_handler_entry':do_irq_entry,
-            'irq_handler_exit':do_irq_exit,
-            'softirq_entry':do_irq_entry,
-            'softirq_exit':do_irq_exit,
-            'exit_syscall':do_syscall_exit
-            }
+    state = TraceState(sched_tids)
 
     for e in event_iter(state):
-        handler = handlers.get(e.name)
-        if not handler and e.name.startswith('sys_'):
-            handler = do_syscall_entry
+        print("Event", e)
+        handler = Handlers.get_handler(e.name)
         if handler:
-            #print(handler.func_name, ":", e)
             for el in handler(e, state):
                 yield el
 
@@ -271,7 +368,9 @@ started_files = set()
 try:
     for sn in range(1,len(tids)+1):
         s_files.append(open("%s/sched%d.json" % (sname, sn), 'w'))
-    for s, block in visual_blocks_iter(tids):
+    for sblock in visual_blocks_iter(tids):
+        #print("sblock", sblock)
+        s, block = sblock
         f = s_files[s]
         if f in started_files:
             print(',', file=f)
